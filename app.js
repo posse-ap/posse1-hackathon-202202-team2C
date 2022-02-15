5cbe1a157071b00d018c5dab9c366aaac939895f
yohei tanaka <yohei0910@keio.jp>
2022-02-15 11:58:58 +0900

    JSの機能を大体やった

--- app.js
+++ app.js
@@ -440,12 +440,12 @@ animes.push(anime);
 
 //タブ切り替え機能
 
-function displaySelected(panel1, panel2, panel3, panel4, panel5, button) {
+function displaySelected(panel1, panel2, panel3, panel4, button) {
     panel1.style.display = 'block';
     panel2.style.display = 'none';
     panel3.style.display = 'none';
     panel4.style.display = 'none';
-    panel5.style.display = 'none';
+    // panel5.style.display = 'none';
     button.setAttribute('aria-selected', true);
 }
 
@@ -454,26 +454,23 @@ function changeDisplay(element) {
     const panel2 = document.getElementById('panel2');
     const panel3 = document.getElementById('panel3');
     const panel4 = document.getElementById('panel4');
-    const panel5 = document.getElementById('panel5');
+    // const panel5 = document.getElementById('panel5');
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
         button.setAttribute('aria-selected', false);
     })
     switch (element) {
         case 'movie':
-            displaySelected(panel1, panel2, panel3, panel4, panel5, buttons[0]);
+            displaySelected(panel1, panel2, panel3, panel4,  buttons[0]);
             break;
         case 'book':
-            displaySelected(panel2, panel1, panel3, panel4, panel5, buttons[1]);
+            displaySelected(panel2, panel1, panel3, panel4,  buttons[1]);
             break;
         case 'music':
-            displaySelected(panel3, panel1, panel2, panel4, panel5, buttons[2]);
-            break;
-        case 'game':
-            displaySelected(panel4, panel1, panel2, panel3, panel5, buttons[3]);
+            displaySelected(panel3, panel1, panel2, panel4,  buttons[2]);
             break;
         default:
-            displaySelected(panel5, panel1, panel2, panel3, panel4, buttons[4]);
+            displaySelected(panel4, panel1, panel2, panel3, buttons[3]);
             break;
     }
 }
