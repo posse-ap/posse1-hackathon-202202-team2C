5de1fbcc53a333626769ba81e73e45a8ff070d6d
Jo Tadano <jotadano@gmail.com>
2022-02-16 13:42:25 +0900

    配色・画像の配置を変更しました

--- app.js
+++ app.js
@@ -327,60 +327,60 @@ $(function () {
 });
 
 // ここから画像のアップロード
-// function previewFile(file) {
-//     // プレビュー画像を追加する要素
-//     const preview = document.getElementById('preview');
+function previewFile(file) {
+    // プレビュー画像を追加する要素
+    const preview = document.getElementById('preview');
 
-//     // FileReaderオブジェクトを作成
-//     const reader = new FileReader();
+    // FileReaderオブジェクトを作成
+    const reader = new FileReader();
 
-//     // ファイルが読み込まれたときに実行する
-//     reader.onload = function (e) {
-//       const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
-//       const img = document.createElement("img"); // img要素を作成
-//       img.src = imageUrl; // 画像のURLをimg要素にセット
-//       preview.appendChild(img); // #previewの中に追加
-//     }
+    // ファイルが読み込まれたときに実行する
+    reader.onload = function (e) {
+      const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
+      const img = document.createElement("img"); // img要素を作成
+      img.src = imageUrl; // 画像のURLをimg要素にセット
+      preview.appendChild(img); // #previewの中に追加
+    }
 
-//     // いざファイルを読み込む
-//     reader.readAsDataURL(file);
-// }
+    // ファイルを読み込む
+    reader.readAsDataURL(file);
+}
 
 
-//   // <input>でファイルが選択されたときの処理
-// const fileInput = document.getElementById('example');
-// const handleFileSelect = () => {
-//     const files = fileInput.files;
-//     for (let i = 0; i < files.length; i++) {
-//     previewFile(files[i]);
-//     }
-// }
-// fileInput.addEventListener('change', handleFileSelect);
+  // <input>でファイルが選択されたときの処理
+const fileInput = document.getElementById('example');
+const handleFileSelect = () => {
+    const files = fileInput.files;
+    for (let i = 0; i < files.length; i++) {
+    previewFile(files[i]);
+    }
+}
+fileInput.addEventListener('change', handleFileSelect);
 
 
 // Background Gradients From
 // https://uigradients.com
 
 
-    //ロードされた際の処理として実施：
-    window.onload = function(){
+    // //ロードされた際の処理として実施：
+    // window.onload = function(){
 
-        //HTML内に画像を表示
-        html2canvas(document.getElementById("target"),{
-          onrendered: function(canvas){
-            //imgタグのsrcの中に、html2canvasがレンダリングした画像を指定する。
-            var imgData = canvas.toDataURL();
-            document.getElementById("result").src = imgData;
-          }
-        });
+    //     //HTML内に画像を表示
+    //     html2canvas(document.getElementById("target"),{
+    //       onrendered: function(canvas){
+    //         //imgタグのsrcの中に、html2canvasがレンダリングした画像を指定する。
+    //         var imgData = canvas.toDataURL();
+    //         document.getElementById("result").src = imgData;
+    //       }
+    //     });
   
-        //ボタンを押下した際にダウンロードする画像を作る
-        html2canvas(document.body,{
-          onrendered: function(canvas){
-            //aタグのhrefにキャプチャ画像のURLを設定
-            var imgData = canvas.toDataURL();
-            document.getElementById("ss").href = imgData;
-          }
-        });
+    //     //ボタンを押下した際にダウンロードする画像を作る
+    //     html2canvas(document.body,{
+    //       onrendered: function(canvas){
+    //         //aタグのhrefにキャプチャ画像のURLを設定
+    //         var imgData = canvas.toDataURL();
+    //         document.getElementById("ss").href = imgData;
+    //       }
+    //     });
   
-      }
+    //   }
